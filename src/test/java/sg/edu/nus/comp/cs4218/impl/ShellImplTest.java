1c7620cef5492d68d014afb443c538d08571691d
Royce <royce963@gmail.com>
2023-02-09 23:05:54 +0800

    Setup maven and gh actions (#25)
    * fix everything
    
    * add placeholders

---
+++ src/test/java/sg/edu/nus/comp/cs4218/impl/ShellImplTest.java
@@ -0,0 +1,12 @@
+package sg.edu.nus.comp.cs4218.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ShellImplTest {
+    @Test
+    void oneEqualsOne() {
+        assertEquals(1, 1);
+    }
+}
