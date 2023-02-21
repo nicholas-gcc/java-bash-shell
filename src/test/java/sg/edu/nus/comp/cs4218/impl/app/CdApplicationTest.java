db59f3082527ba57ffee35b47dc533788969295e
ongweisheng <ongweisheng30@gmail.com>
2023-02-21 23:23:26 +0800

    Add test case for path exist and does not

--- src/test/java/sg/edu/nus/comp/cs4218/impl/app/CdApplicationTest.java
+++ src/test/java/sg/edu/nus/comp/cs4218/impl/app/CdApplicationTest.java
@@ -2,6 +2,7 @@ package sg.edu.nus.comp.cs4218.impl.app;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import sg.edu.nus.comp.cs4218.Environment;
 import sg.edu.nus.comp.cs4218.exception.CdException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,5 +22,20 @@ public class CdApplicationTest {
         String string = " ";
         Exception exception = assertThrows(Exception.class, () -> cdApplication.changeToDirectory(string));
         assertEquals("cd: " + ERR_NO_ARGS, exception.getMessage());
+    }
+
+    @Test
+    void cd_PathDoesNotExist_ShouldCdCorrectly() throws CdException {
+        String string = "blah";
+        Exception exception = assertThrows(Exception.class, () -> cdApplication.changeToDirectory(string));
+        assertEquals("cd: " + ERR_FILE_NOT_FOUND, exception.getMessage());
+    }
+
+    @Test
+    void cd_PathExist_ShouldCdCorrectly() throws CdException {
+        String string = "src/test";
+        String initialDirectory = Environment.currentDirectory;
+        cdApplication.changeToDirectory(string);
+        assertEquals(initialDirectory + "/src/test", Environment.currentDirectory);
     }
 }
