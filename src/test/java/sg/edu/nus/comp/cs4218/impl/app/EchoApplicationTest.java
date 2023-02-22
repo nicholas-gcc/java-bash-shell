f1fd22f4cf655f672c6f4ee4ebe0c23bdfd46809
ongweisheng <ongweisheng30@gmail.com>
2023-02-22 20:09:56 +0800

    Add test case for long string

--- src/test/java/sg/edu/nus/comp/cs4218/impl/app/EchoApplicationTest.java
+++ src/test/java/sg/edu/nus/comp/cs4218/impl/app/EchoApplicationTest.java
@@ -51,4 +51,16 @@ public class EchoApplicationTest {
         String result = echoApplication.constructResult(array);
         assertEquals("!@#$%^&*()", result);
     }
+
+    @Test
+    void echo_LongString_ShouldEchoCorrectly() throws EchoException {
+        String [] array = {"PqZu6co8rw7SWvSqreE56i09RKf2LYfrGhsGPqbOPuqdrgZ0adHF28LWi2RjIpJtCtc6z9HEYLMVezXMzdUy1r" +
+                "UVT3V3NZVpYVoigZP6yIp4wSqZ3M9D8Ail8NLeSLGUNyEL0ZwKA3agPity5jk3K3eeikT0YnN3y5a0IR9U858LOANl0hL5gew" +
+                "hgOA225JqW3nuy69U2Xfh6DXA8AY4qSn5zMbCK1X7VAF2qiFBt1kCDhpuhwJCa7yB147GAhTFg0SN3dGNWEeOQVPjo8T03Fih" +
+                "QfrCfkZUNpllTU4DpBdlizRxCNVpRjh2UxpZo2AD6aXkjKKKJw4sRgMTAizXxq489837f2bkylQBX3hCx2YppTiw5rZXbkKu7" +
+                "H4FwJAC85J6vvwFsMlUfxPoustRtGVukFtIIRKnL7omUOAxaSH515m043UT2ckSKSGbzcSc0bUy1V6X28rI6ZnnINV8J6uZ3X" +
+                "pc5DisJSEfm4WadhZC7fqPkPVj"};
+        String result = echoApplication.constructResult(array);
+        assertEquals(array[0], result);
+    }
 }
