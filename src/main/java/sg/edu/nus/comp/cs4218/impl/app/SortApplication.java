52ebb9d911100ce66247b8e91811bc92e908c314
nicholas-gcc <nicholasgcc@gmail.com>
2023-03-14 13:29:56 +0800

    Fix wildcard imports

--- src/main/java/sg/edu/nus/comp/cs4218/impl/app/SortApplication.java
+++ src/main/java/sg/edu/nus/comp/cs4218/impl/app/SortApplication.java
@@ -5,9 +5,18 @@ import sg.edu.nus.comp.cs4218.exception.SortException;
 import sg.edu.nus.comp.cs4218.impl.app.args.SortArguments;
 import sg.edu.nus.comp.cs4218.impl.util.IOUtils;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStream;
+
 import java.math.BigInteger;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.*;
 import static sg.edu.nus.comp.cs4218.impl.util.StringUtils.STRING_NEWLINE;
