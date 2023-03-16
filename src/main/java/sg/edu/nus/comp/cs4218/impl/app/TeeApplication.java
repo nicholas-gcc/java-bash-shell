7b4b7625c516761887051d157ade306cedc4377d
Lim Zi Yang <e0406401@u.nus.edu>
2023-03-17 00:21:04 +0800

    fix pmd violations for PreserveStackTrace

--- src/main/java/sg/edu/nus/comp/cs4218/impl/app/TeeApplication.java
+++ src/main/java/sg/edu/nus/comp/cs4218/impl/app/TeeApplication.java
@@ -55,7 +55,7 @@ public class TeeApplication implements TeeInterface {
         try {
             parser.parse(args);
         } catch (InvalidArgsException e) {
-            throw new TeeException(e.getMessage());//NOPMD
+            throw new TeeException(e.getMessage(), e);
         }
 
         boolean isAppend = parser.isAppend();
@@ -66,7 +66,7 @@ public class TeeApplication implements TeeInterface {
         try {
             stdout.write(output.getBytes());
         } catch (Exception e) {
-            throw new TeeException(ERR_WRITE_STREAM);//NOPMD
+            throw new TeeException(ERR_WRITE_STREAM, e);
         }
     }
 
@@ -74,7 +74,7 @@ public class TeeApplication implements TeeInterface {
         try {
             return IOUtils.getLinesFromInputStream(stdin);
         } catch (Exception e) {
-            throw new TeeException(e.getMessage());//NOPMD
+            throw new TeeException(e.getMessage(), e);
         }
     }
 
@@ -82,7 +82,7 @@ public class TeeApplication implements TeeInterface {
         try {
             FileSystemUtils.writeStrToFile(isAppend, content, filename);
         } catch (Exception e) {
-            throw new TeeException(e.getMessage());//NOPMD
+            throw new TeeException(e.getMessage(), e);
         }
     }
 
