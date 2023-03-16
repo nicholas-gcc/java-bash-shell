b9afb4221b906d1df9c7f85edd9ee7d148ca0d0b
nicholas-gcc <nicholasgcc@gmail.com>
2023-03-17 00:24:59 +0800

    Check error messages in unit tests, identify and fix bug on invalid flags for SortArguments

--- src/test/java/sg/edu/nus/comp/cs4218/impl/app/SortApplicationTest.java
+++ src/test/java/sg/edu/nus/comp/cs4218/impl/app/SortApplicationTest.java
@@ -15,6 +15,12 @@ import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_FILE_NOT_FOUND;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_INVALID_FLAG;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_IS_DIR;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NULL_ARGS;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NULL_STREAMS;
+import static sg.edu.nus.comp.cs4218.impl.util.StringUtils.STRING_NEWLINE;
 
 class SortApplicationTest {
 
@@ -29,11 +35,11 @@ class SortApplicationTest {
 
     @Test
     void sortFromFiles_ValidTextFile_CorrectOutput() throws Exception {
-        String input = "abc" + System.lineSeparator() + "zzz" + System.lineSeparator()
-                + "ccc" + System.lineSeparator() + "bbb" + System.lineSeparator() + "aaa";
+        String input = "abc" + STRING_NEWLINE + "zzz" + STRING_NEWLINE
+                + "ccc" + STRING_NEWLINE + "bbb" + STRING_NEWLINE + "aaa";
 
-        String expectedOutput = "aaa" + System.lineSeparator() + "abc" + System.lineSeparator() +
-                "bbb" + System.lineSeparator() + "ccc" + System.lineSeparator() + "zzz";
+        String expectedOutput = "aaa" + STRING_NEWLINE + "abc" + STRING_NEWLINE +
+                "bbb" + STRING_NEWLINE + "ccc" + STRING_NEWLINE + "zzz";
 
         Path inputFile = createTempFileWithContent(input);
         String[] fileNames = new String[]{inputFile.toString()};
@@ -47,27 +53,32 @@ class SortApplicationTest {
     void sortFromFiles_InvalidFile_ShouldThrow() {
         String[] fileNames = new String[]{"invalid-file"};
 
-        assertThrows(SortException.class, () -> {
+        Throwable err = assertThrows(SortException.class, () -> {
             sortApp.sortFromFiles(false, false, false, fileNames);
         });
+
+        assertEquals("sort: " + ERR_FILE_NOT_FOUND, err.getMessage());
+
     }
 
     @Test
     void sortFromFiles_InputIsDirectory_ShouldThrow() {
         String[] fileNames = new String[]{tempDir.toString()};
 
-        assertThrows(SortException.class, () -> {
+        Throwable err = assertThrows(SortException.class, () -> {
             sortApp.sortFromFiles(false, false, false, fileNames);
         });
+
+        assertEquals("sort: " + ERR_IS_DIR, err.getMessage());
     }
 
     @Test
     void sortFromStdin_ValidInput_CorrectOutput() throws Exception {
-        String input = "abc" + System.lineSeparator() + "zzz" + System.lineSeparator()
-                + "ccc" + System.lineSeparator() + "bbb" + System.lineSeparator() + "aaa";
+        String input = "abc" + STRING_NEWLINE + "zzz" + STRING_NEWLINE
+                + "ccc" + STRING_NEWLINE + "bbb" + STRING_NEWLINE + "aaa";
 
-        String expectedOutput = "aaa" + System.lineSeparator() + "abc" + System.lineSeparator() +
-                "bbb" + System.lineSeparator() + "ccc" + System.lineSeparator() + "zzz";
+        String expectedOutput = "aaa" + STRING_NEWLINE + "abc" + STRING_NEWLINE +
+                "bbb" + STRING_NEWLINE + "ccc" + STRING_NEWLINE + "zzz";
 
         InputStream inputStream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
 
@@ -78,11 +89,11 @@ class SortApplicationTest {
 
     @Test
     void sortFromStdin_ValidInputReverse_CorrectOutput() throws Exception {
-        String input = "abc" + System.lineSeparator() + "zzz" + System.lineSeparator()
-                + "ccc" + System.lineSeparator() + "bbb" + System.lineSeparator() + "aaa";
+        String input = "abc" + STRING_NEWLINE + "zzz" + STRING_NEWLINE
+                + "ccc" + STRING_NEWLINE + "bbb" + STRING_NEWLINE + "aaa";
 
-        String expectedOutput = "zzz" + System.lineSeparator() + "ccc" + System.lineSeparator()
-                + "bbb" + System.lineSeparator() + "abc" + System.lineSeparator() + "aaa";
+        String expectedOutput = "zzz" + STRING_NEWLINE + "ccc" + STRING_NEWLINE
+                + "bbb" + STRING_NEWLINE + "abc" + STRING_NEWLINE + "aaa";
 
         InputStream inputStream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
 
@@ -103,24 +114,27 @@ class SortApplicationTest {
 
     @Test
     void sortFromStdin_NullInput_ShouldThrow() {
-        assertThrows(SortException.class, () -> {
+        Throwable err = assertThrows(SortException.class, () -> {
             sortApp.sortFromStdin(false, false, false, null);
         });
+
+        assertEquals("sort: " + ERR_NULL_STREAMS, err.getMessage());
+
     }
 
     @Test
     void run_ValidInputFilesAndOutputStream_CorrectOutput() throws Exception {
         // should combine the items of two files and sort them after merging
-        String input1 = "abc" + System.lineSeparator() + "zzz" + System.lineSeparator()
-                + "ccc" + System.lineSeparator() + "bbb" + System.lineSeparator() + "aaa";
+        String input1 = "abc" + STRING_NEWLINE + "zzz" + STRING_NEWLINE
+                + "ccc" + STRING_NEWLINE + "bbb" + STRING_NEWLINE + "aaa";
 
-        String input2 = "def" + System.lineSeparator() + "efg" + System.lineSeparator()
-                + "bcd" + System.lineSeparator() + "aaa";
+        String input2 = "def" + STRING_NEWLINE + "efg" + STRING_NEWLINE
+                + "bcd" + STRING_NEWLINE + "aaa";
 
-        String expectedOutput = "aaa" + System.lineSeparator() + "aaa" + System.lineSeparator()
-                + "abc" + System.lineSeparator() + "bbb" + System.lineSeparator() + "bcd" +
-                System.lineSeparator() + "ccc" + System.lineSeparator() + "def" + System.lineSeparator()
-                + "efg" + System.lineSeparator() + "zzz";
+        String expectedOutput = "aaa" + STRING_NEWLINE + "aaa" + STRING_NEWLINE
+                + "abc" + STRING_NEWLINE + "bbb" + STRING_NEWLINE + "bcd" +
+                STRING_NEWLINE + "ccc" + STRING_NEWLINE + "def" + STRING_NEWLINE
+                + "efg" + STRING_NEWLINE + "zzz";
 
         // create the files
         InputStream inputStream = new ByteArrayInputStream(input1.getBytes(StandardCharsets.UTF_8));
@@ -132,20 +146,20 @@ class SortApplicationTest {
         sortApp.run(new String[]{"-n", "-f", inputFile1.toString(), inputFile2.toString()}, inputStream, outputStream);
         String result = new String(Files.readAllBytes(outputFile));
 
-        assertEquals(expectedOutput + System.lineSeparator(), result);
+        assertEquals(expectedOutput + STRING_NEWLINE, result);
     }
 
     @Test
     void run_ValidInputFilesAndOutputStreamReverse_CorrectOutput() throws Exception {
-        String input1 = "abc" + System.lineSeparator() + "zzz" + System.lineSeparator()
-                + "ccc" + System.lineSeparator() + "bbb" + System.lineSeparator() + "aaa";
+        String input1 = "abc" + STRING_NEWLINE + "zzz" + STRING_NEWLINE
+                + "ccc" + STRING_NEWLINE + "bbb" + STRING_NEWLINE + "aaa";
 
-        String input2 = "def" + System.lineSeparator() + "efg" + System.lineSeparator()
-                + "bcd" + System.lineSeparator() + "aaa";
+        String input2 = "def" + STRING_NEWLINE + "efg" + STRING_NEWLINE
+                + "bcd" + STRING_NEWLINE + "aaa";
 
-        String expectedOutput = "zzz" + System.lineSeparator() + "efg" + System.lineSeparator()
-                + "def" + System.lineSeparator() + "ccc" + System.lineSeparator() + "bcd" + System.lineSeparator()
-                + "bbb" + System.lineSeparator() + "abc" + System.lineSeparator() + "aaa" + System.lineSeparator() + "aaa";
+        String expectedOutput = "zzz" + STRING_NEWLINE + "efg" + STRING_NEWLINE
+                + "def" + STRING_NEWLINE + "ccc" + STRING_NEWLINE + "bcd" + STRING_NEWLINE
+                + "bbb" + STRING_NEWLINE + "abc" + STRING_NEWLINE + "aaa" + STRING_NEWLINE + "aaa";
 
         // create the files
         InputStream inputStream = new ByteArrayInputStream(input1.getBytes(StandardCharsets.UTF_8));
@@ -157,30 +171,37 @@ class SortApplicationTest {
         sortApp.run(new String[]{"-n", "-r", "-f", inputFile1.toString(), inputFile2.toString()}, inputStream, outputStream);
         String result = new String(Files.readAllBytes(outputFile));
 
-        assertEquals(expectedOutput + System.lineSeparator(), result);
+        assertEquals(expectedOutput + STRING_NEWLINE, result);
     }
 
     @Test
     void run_NullArgs_ShouldThrow() {
-        assertThrows(SortException.class, () -> {
+        Throwable err = assertThrows(SortException.class, () -> {
             sortApp.run(null, null, null);
         });
+
+        assertEquals("sort: " + ERR_NULL_ARGS, err.getMessage());
+
     }
 
     @Test
     void run_NullOutputStream_ShouldThrow() {
-        assertThrows(SortException.class, () -> {
-            sortApp.run(new String[]{}, null, null);
+        Throwable err = assertThrows(SortException.class, () -> {
+            sortApp.sortFromStdin(false, false, false, null);
         });
+
+        assertEquals("sort: " + ERR_NULL_STREAMS, err.getMessage());
     }
 
     @Test
     void run_InvalidArguments_ShouldThrow() throws IOException {
         String[] invalidArgs = new String[]{"-invalid", "filename.txt"};
         Path outputFile = Files.createTempFile(tempDir, "output", ".txt");
-        assertThrows(SortException.class, () -> {
+        Throwable err = assertThrows(SortException.class, () -> {
             sortApp.run(invalidArgs, null, Files.newOutputStream(outputFile));
         });
+
+        assertEquals("sort: " + ERR_INVALID_FLAG + ": " + "-invalid", err.getMessage());
     }
 
     private Path createTempFileWithContent(String content) throws IOException {
