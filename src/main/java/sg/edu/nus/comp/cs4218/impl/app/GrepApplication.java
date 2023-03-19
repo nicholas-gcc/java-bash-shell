0c78292fb146540eec171269d77e74a5a02531c7
Lim Zi Yang <e0406401@u.nus.edu>
2023-03-19 23:15:44 +0800

    clean up code

--- src/main/java/sg/edu/nus/comp/cs4218/impl/app/GrepApplication.java
+++ src/main/java/sg/edu/nus/comp/cs4218/impl/app/GrepApplication.java
@@ -12,7 +12,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
-import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.*;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_FILE_DIR_NOT_FOUND;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_FILE_NOT_FOUND;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_INVALID_REGEX;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NO_INPUT;
 import static sg.edu.nus.comp.cs4218.impl.util.StringUtils.CHAR_FLAG_PREFIX;
 import static sg.edu.nus.comp.cs4218.impl.util.StringUtils.STRING_NEWLINE;
 
@@ -162,7 +165,7 @@ public class GrepApplication implements GrepInterface {
             String pattern = grepArgs.getGrepArguments(args, inputFiles);
             GrepArguments.validate(pattern);
 
-            String result = "";
+            String result;
 
             if (stdin == null && inputFiles.isEmpty()) {
                 throw new Exception(ERR_NO_INPUT);
