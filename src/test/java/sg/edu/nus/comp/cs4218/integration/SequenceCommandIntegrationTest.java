5c565e498147e29c6c527972d51fdefb39cb0870
Cheng Jiyuqing <chengjiyuqing@gmail.com>
2023-03-20 02:09:01 +0800

    Revert "trial: combine delete file and close stream"
    This reverts commit 9a8e117913ae69d95551a4b2feb703b1354727ea.

--- src/test/java/sg/edu/nus/comp/cs4218/integration/SequenceCommandIntegrationTest.java
+++ src/test/java/sg/edu/nus/comp/cs4218/integration/SequenceCommandIntegrationTest.java
@@ -9,7 +9,6 @@ import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
 import sg.edu.nus.comp.cs4218.exception.ShellException;
 import sg.edu.nus.comp.cs4218.impl.ShellImpl;
 import sg.edu.nus.comp.cs4218.impl.util.FileSystemUtils;
-import sg.edu.nus.comp.cs4218.impl.util.IOUtils;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -54,16 +53,15 @@ public class SequenceCommandIntegrationTest {
         outputStream = new ByteArrayOutputStream();
     }
 
-//    @AfterEach
-//    void reset() throws IOException {
-//        Environment.currentDirectory = CWD;
-//        inputStream.close();
-//        outputStream.close();
-//    }
+    @AfterEach
+    void reset() throws IOException {
+        Environment.currentDirectory = CWD;
+        inputStream.close();
+        outputStream.close();
+    }
 
     @AfterEach
     void deleteFiles() throws Exception {
-
         if (FileSystemUtils.fileOrDirExist(NEW_FILE)) {
             FileSystemUtils.deleteFileOrDir(NEW_FILE);
         }
@@ -76,9 +74,6 @@ public class SequenceCommandIntegrationTest {
         if (FileSystemUtils.fileOrDirExist(DIR + CHAR_FILE_SEP + NEW_FILE2)) {
             FileSystemUtils.deleteFileOrDir(DIR + CHAR_FILE_SEP + NEW_FILE2);
         }
-        Environment.currentDirectory = CWD;
-        inputStream.close();
-        outputStream.close();
     }
 
     @Test
